56d8ac76ff719c160ee398adb8e5b06e4918ceee
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-06 15:32:51 -0300

    feat: Implementação do RestClienteException

--- lib/app/core/rest_client/rest_client.dart
+++ lib/app/core/rest_client/rest_client.dart
@@ -8,4 +8,15 @@ class RestClient extends GetConnect {
   }
 }
 
-// Exception RestClientException
+class RestClientException implements Exception {
+  final int? code;
+  final String message;
+
+  RestClientException({
+    this.code,
+    required this.message,
+  });
+
+  @override
+  String toString() => 'RestClientException(code : $code, message: $message)';
+}
