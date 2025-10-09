f1c2169f418b5115389345369e209fd49e578df6
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-09 09:01:19 -0300

    feat: Drawer funcional

---
+++ lib/app/core/ui/widgets/galegos_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:restaurante_galegos/app/core/service/auth_service.dart';
+
+class GalegosDrawer extends StatelessWidget {
+  const GalegosDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: context.widthTransformer(reducedBy: 20),
+      elevation: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          ButtonDrawer(
+            title: 'Configurações',
+            onTap: () {},
+          ),
+          Divider(),
+          ButtonDrawer(
+            title: 'Horário de funcionamento',
+            onTap: () {},
+          ),
+          Divider(),
+          ButtonDrawer(
+            title: 'Sobre nós',
+            onTap: () {},
+          ),
+          Divider(),
+          ButtonDrawer(
+            title: 'Sair',
+            onTap: AuthService().logout,
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonDrawer extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const ButtonDrawer({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        constraints: BoxConstraints(
+          minHeight: 100,
+        ),
+        width: context.width,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              title,
+              // textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
