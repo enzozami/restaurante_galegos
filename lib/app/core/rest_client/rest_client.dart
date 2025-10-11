eaa0b42a6108de704fbfc680044d0713b44035ed
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-11 10:33:54 -0300

    configuração do IP do pc (casa)

--- lib/app/core/rest_client/rest_client.dart
+++ lib/app/core/rest_client/rest_client.dart
@@ -1,7 +1,9 @@
 import 'package:get/get_connect/connect.dart';
 
 class RestClient extends GetConnect {
-  final _baseUrl = 'http://10.24.24.247:8080';
+  // final _baseUrl = 'http://10.24.24.247:8080'; // EMPRESA
+  // final _baseUrl = 'http://192.168.15.7:8080'; // pc de casa
+  final _baseUrl = 'http://192.168.100.49:8080'; // restaurante
 
   RestClient() {
     httpClient.baseUrl = _baseUrl;
