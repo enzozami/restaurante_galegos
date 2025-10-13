1c1cfb479ed55141fa25c1aa3aae3d749da950a5
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-13 17:10:41 -0300

    refactor: O cabeçalho da pagina de marmitas está configurado [apresentando na tela, ainda falta fazer o filtro do dia]

--- lib/app/modules/lunchboxes/widgets/lunchboxes_header.dart
+++ lib/app/modules/lunchboxes/widgets/lunchboxes_header.dart
@@ -1,9 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
-import 'package:restaurante_galegos/app/models/day_model.dart';
 import 'package:restaurante_galegos/app/modules/lunchboxes/lunchboxes_controller.dart';
 import 'package:restaurante_galegos/app/modules/lunchboxes/widgets/button_tag.dart';
 
@@ -17,20 +14,18 @@ class LunchboxesHeader extends GetView<LunchboxesController> {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Obx(() {
-          final menus = controller.menu;
+          final day = controller.days;
 
-          final menuDay = menus.map((e) => e).toList();
-          log('$menuDay');
-
-          for (var menu in menuDay) {
-            final listData = [
-              ...menu.day.map((e) {
-                return Row(
-                  children: ButtonTag(model: e, onPressed: () {}),
-                );
-              })
-            ];
-          }
+          return Row(
+            children: day
+                .map(
+                  (d) => ButtonTag(
+                    model: d,
+                    onPressed: () {},
+                  ),
+                )
+                .toList(),
+          );
         }),
       ),
     );
