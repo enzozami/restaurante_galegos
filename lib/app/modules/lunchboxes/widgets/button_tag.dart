9d7b8ba046c22ed98c34f6297255c70e3ab7eab0
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-14 15:27:46 -0300

    feat: Filtro do tamanho definindo o preço

--- lib/app/modules/lunchboxes/widgets/button_tag.dart
+++ lib/app/modules/lunchboxes/widgets/button_tag.dart
@@ -14,34 +14,30 @@ class ButtonTag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        InkWell(
-          onTap: onPressed,
-          child: Container(
-            margin: EdgeInsets.all(5),
-            padding: EdgeInsets.all(15),
-            constraints: BoxConstraints(
-              minHeight: 40,
-              minWidth: 130,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(),
-              borderRadius: BorderRadius.circular(30),
-              color: isSelected ? Colors.amber : Colors.black,
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                model,
-                style: TextStyle(
-                  color: isSelected ? Colors.amber : Colors.amber,
-                ),
-              ),
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.all(15),
+        constraints: BoxConstraints(
+          minHeight: 40,
+          minWidth: 130,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(30),
+          color: isSelected ? Colors.amber : Colors.black,
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            model,
+            style: TextStyle(
+              color: isSelected ? Colors.black : Colors.amber,
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
