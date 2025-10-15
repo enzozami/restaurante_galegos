6e4a2b65e28edeb3bce20eac7885383c8e8c3885
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-15 08:40:55 -0300

    style: Melhorando o visual

--- lib/app/modules/lunchboxes/widgets/alimentos_widget.dart
+++ lib/app/modules/lunchboxes/widgets/alimentos_widget.dart
@@ -15,49 +15,52 @@ class AlimentosWidget extends GetView<LunchboxesController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      final selectedSize = controller.sizeSelected.value;
-      final price = selectedSize != '' ? alimentoModel.pricePerSize[selectedSize] ?? 0 : null;
-      return Material(
-        child: Container(
-          constraints: BoxConstraints(
-            minHeight: 100,
-          ),
-          width: context.width,
-          child: InkWell(
-            splashColor: Colors.amber,
-            onTap: () => log('Item clicado: ${alimentoModel.name} - $price'),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ListTile(
-                  title: Text(
-                    alimentoModel.name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Obx(() {
+        final selectedSize = controller.sizeSelected.value;
+        final price = selectedSize != '' ? alimentoModel.pricePerSize[selectedSize] : null;
+        return Material(
+          child: Container(
+            constraints: BoxConstraints(
+              minHeight: 100,
+            ),
+            width: context.width,
+            child: InkWell(
+              splashColor: Colors.amber,
+              onTap: () => log('Item clicado: ${alimentoModel.name} - $price'),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListTile(
+                    title: Text(
+                      alimentoModel.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Text(alimentoModel.description),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (price != null)
+                          Text(
+                            FormatterHelper.formatCurrency(price),
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                      ],
                     ),
                   ),
-                  subtitle: Text(alimentoModel.description),
-                  trailing: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (price != null)
-                        Text(
-                          FormatterHelper.formatCurrency(price),
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    });
+        );
+      }),
+    );
   }
 }
