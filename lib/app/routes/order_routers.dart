7eba6e04649b84b743c2e08eee48e7be82c4b44b
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-15 09:20:10 -0300

    refactor: Refatoração do código

--- lib/app/routes/order_routers.dart
+++ lib/app/routes/order_routers.dart
@@ -1,15 +1,15 @@
 import 'package:get/get.dart';
-import 'package:restaurante_galegos/app/modules/order/order_bindings.dart';
-import 'package:restaurante_galegos/app/modules/order/order_page.dart';
+import 'package:restaurante_galegos/app/modules/order/shopping_card/shopping_card_bindings.dart';
+import 'package:restaurante_galegos/app/modules/order/shopping_card/shopping_card_page.dart';
 
 class OrderRouters {
   OrderRouters._();
 
   static final routers = <GetPage>[
     GetPage(
-      name: '/order',
-      binding: OrderBindings(),
-      page: () => OrderPage(),
+      name: '/order/shopping',
+      binding: ShoppingCardBindings(),
+      page: () => ShoppingCardPage(),
     )
   ];
 }
--- lib/app/routes/shoppgin_card_routers.dart
+++ lib/app/routes/shoppgin_card_routers.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
-import 'package:restaurante_galegos/app/modules/shopping_card/shopping_card_bindings.dart';
-import 'package:restaurante_galegos/app/modules/shopping_card/shopping_card_page.dart';
+import 'package:restaurante_galegos/app/modules/order/shopping_card/shopping_card_bindings.dart';
+import 'package:restaurante_galegos/app/modules/order/shopping_card/shopping_card_page.dart';
 
 class ShoppginCardRouters {
   ShoppginCardRouters._();
