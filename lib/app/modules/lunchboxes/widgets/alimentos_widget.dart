977dc8fb883213c02b85c965b8f093114d942b03
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-16 10:30:01 -0300

    style: Estilização finalizada da pagina de Marmita

--- lib/app/modules/lunchboxes/widgets/alimentos_widget.dart
+++ lib/app/modules/lunchboxes/widgets/alimentos_widget.dart
@@ -16,51 +16,55 @@ class AlimentosWidget extends GetView<LunchboxesController> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Obx(() {
         final selectedSize = controller.sizeSelected.value;
         final price = selectedSize != '' ? alimentoModel.pricePerSize[selectedSize] : null;
-        return Material(
-          child: Container(
-            constraints: BoxConstraints(
-              minHeight: 100,
-            ),
-            width: context.width,
-            child: InkWell(
-              splashColor: Colors.amber,
-              onTap: () {
-                controller.foodSelect(alimentoModel);
-                controller.addFood();
-                controller.addFoodShoppingCard();
-                log('Item clicado: ${alimentoModel.name} - $price');
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ListTile(
-                    title: Text(
-                      alimentoModel.name,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+        return Visibility(
+          visible: (selectedSize?.isNotEmpty ?? false),
+          child: Material(
+            child: Container(
+              constraints: BoxConstraints(
+                minHeight: 100,
+              ),
+              width: context.width,
+              child: InkWell(
+                splashColor: Colors.amber,
+                onTap: () {
+                  controller.foodSelect(alimentoModel);
+                  controller.addFood();
+                  controller.addFoodShoppingCard();
+                  log('Item clicado: ${alimentoModel.name} - $price');
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      title: Text(
+                        alimentoModel.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      subtitle: Text(alimentoModel.description),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (price != null)
+                            Text(
+                              FormatterHelper.formatCurrency(price),
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                        ],
                       ),
                     ),
-                    subtitle: Text(alimentoModel.description),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (price != null)
-                          Text(
-                            FormatterHelper.formatCurrency(price),
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                ],
+                    Divider(),
+                  ],
+                ),
               ),
             ),
           ),
