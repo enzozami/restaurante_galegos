de6e040d1a7af0043ba179b19cb64a19e914080d
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-22 09:59:10 -0300

    refactor: Alterando o botão de finalizar para apenas apresentar uma snackbar (para cliente)

--- lib/app/modules/order/shopping_card/shopping_card_controller.dart
+++ lib/app/modules/order/shopping_card/shopping_card_controller.dart
@@ -6,6 +6,7 @@ import 'package:restaurante_galegos/app/core/mixins/messages_mixin.dart';
 import 'package:restaurante_galegos/app/core/service/auth_service.dart';
 import 'package:restaurante_galegos/app/models/item_carrinho.dart';
 import 'package:restaurante_galegos/app/models/shopping_card_model.dart';
+import 'package:restaurante_galegos/app/modules/home/home_controller.dart';
 import 'package:restaurante_galegos/app/services/order/order_services.dart';
 import 'package:restaurante_galegos/app/services/shopping/shopping_card_services.dart';
 
@@ -70,9 +71,47 @@ class ShoppingCardController extends GetxController with LoaderMixin, MessagesMi
     );
   }
 
-  Future<void> createOrder({required String address}) async {
+// PARTE PARA ADMIN
+  // Future<void> createOrder({required String address}) async {
+  //   try {
+  //     _loading.toggle();
+  //     final user = _authService.getUserId();
+  //     log('USUÁRIO: $user');
+  //     final order = ItemCarrinho(
+  //       userId: user!,
+  //       address: address,
+  //       items: _cardServices.productsSelected,
+  //       quantity: quantity,
+  //     );
+
+  //     log('ORDER-json: ${order.items.map((e) => e.toJson())}');
+
+  //     var finished = await _orderServices.createOrder(order);
+  //     finished = finished.copyWith(amountToPay: totalPay());
+  //     log('ORDER-FINALIZADO: ${finished.toJson()}');
+
+  //     _loading.toggle();
+  //     clear();
+  //     await Get.offNamed('/order/finished', arguments: finished);
+  //   } catch (e, s) {
+  //     _loading.toggle();
+  //     log('Erro ao carregar produtos no carrinho', error: e, stackTrace: s);
+  //     _message(
+  //       MessageModel(
+  //         title: 'Erro',
+  //         message: 'Erro ao carregar produtos no carrinho',
+  //         type: MessageType.error,
+  //       ),
+  //     );
+  //   } finally {
+  //     _loading(false);
+  //   }
+  // }
+  final homeController = Get.find<HomeController>();
+
+  Future<bool> createOrder({required String address}) async {
     try {
-      _loading.toggle();
+      _loading(true);
       final user = _authService.getUserId();
       log('USUÁRIO: $user');
       final order = ItemCarrinho(
@@ -81,18 +120,16 @@ class ShoppingCardController extends GetxController with LoaderMixin, MessagesMi
         items: _cardServices.productsSelected,
         quantity: quantity,
       );
-
-      log('ORDER-json: ${order.items.map((e) => e.toJson())}');
-
-      var finished = await _orderServices.createOrder(order);
-      finished = finished.copyWith(amountToPay: totalPay());
-      log('ORDER-FINALIZADO: ${finished.toJson()}');
-
-      _loading.toggle();
+      await _orderServices.createOrder(order);
       clear();
-      await Get.offNamed('/order/finished', arguments: finished);
+
+      _loading(false);
+
+      homeController.selectedIndex = 0;
+
+      return true;
     } catch (e, s) {
-      _loading.toggle();
+      _loading(false);
       log('Erro ao carregar produtos no carrinho', error: e, stackTrace: s);
       _message(
         MessageModel(
@@ -101,6 +138,7 @@ class ShoppingCardController extends GetxController with LoaderMixin, MessagesMi
           type: MessageType.error,
         ),
       );
+      return false;
     } finally {
       _loading(false);
     }
