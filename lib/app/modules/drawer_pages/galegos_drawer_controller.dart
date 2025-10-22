8dd45ff90ddba152a0d853a83a71f0759174e9d6
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-22 15:24:12 -0300

    feat: Tela de perfil sendo configurada

--- lib/app/modules/drawer_pages/galegos_drawer_controller.dart
+++ lib/app/modules/drawer_pages/galegos_drawer_controller.dart
@@ -1,3 +1,57 @@
 import 'package:get/get.dart';
+import 'package:restaurante_galegos/app/core/mixins/loader_mixin.dart';
+import 'package:restaurante_galegos/app/core/mixins/messages_mixin.dart';
+import 'package:restaurante_galegos/app/core/service/auth_service.dart';
+import 'package:restaurante_galegos/app/services/user/user_services.dart';
 
-class GalegosDrawerController extends GetxController {}
+class GalegosDrawerController extends GetxController with LoaderMixin, MessagesMixin {
+  final UserServices _userServices;
+  final AuthService _authService;
+
+  final _loading = false.obs;
+  final _message = Rxn<MessageModel>();
+  final _isSelected = false.obs;
+  final _isObscure = true.obs;
+
+  bool get isSelected => _isSelected.value;
+  bool get isObscure => _isObscure.value;
+
+  var name = ''.obs;
+  var password = ''.obs;
+
+  GalegosDrawerController({
+    required UserServices userServices,
+    required AuthService authService,
+  })  : _userServices = userServices,
+        _authService = authService;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loaderListener(_loading);
+    messageListener(_message);
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    getUser();
+  }
+
+  void isSelect() {
+    _isSelected.toggle();
+  }
+
+  void obscure() {
+    _isObscure.toggle();
+  }
+
+  Future<void> getUser() async {
+    final userId = _authService.getUserId();
+    if (userId != null) {
+      final userData = await _userServices.getUser(id: userId);
+      name.value = userData.name;
+      password.value = userData.password;
+    }
+  }
+}
