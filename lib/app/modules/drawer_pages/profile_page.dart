539791057d29d3b9949d332a242eaf9bf1e7a69d
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-23 10:44:40 -0300

    feat: Criada tela de perfil

--- lib/app/modules/drawer_pages/profile_page.dart
+++ lib/app/modules/drawer_pages/profile_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:restaurante_galegos/app/core/ui/galegos_state.dart';
 import 'package:restaurante_galegos/app/core/ui/widgets/galegos_app_bar.dart';
+import 'package:restaurante_galegos/app/core/ui/widgets/galegos_button_default.dart';
 import 'package:restaurante_galegos/app/modules/drawer_pages/galegos_drawer_controller.dart';
 import 'package:restaurante_galegos/app/modules/drawer_pages/widgets/profile_data.dart';
+import 'package:validatorless/validatorless.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -18,9 +20,23 @@ class _ProfilePageState extends GalegosState<ProfilePage, GalegosDrawerControlle
   final _newPasswordEC = TextEditingController();
 
   @override
+  void dispose() {
+    super.dispose();
+    _newNameEC.dispose();
+    _newPasswordEC.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GalegosAppBar(),
+      appBar: GalegosAppBar(
+        icon: Obx(() {
+          return IconButton(
+            onPressed: controller.isSelect,
+            icon: controller.isSelected ? Icon(Icons.close) : Icon(Icons.edit),
+          );
+        }),
+      ),
       body: LayoutBuilder(
         builder: (context, constraints) {
           return SingleChildScrollView(
@@ -30,15 +46,26 @@ class _ProfilePageState extends GalegosState<ProfilePage, GalegosDrawerControlle
                 minWidth: constraints.maxWidth,
               ),
               child: IntrinsicHeight(
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Text('DADOS DO USUÁRIO'),
-                    ),
-                    Obx(() {
-                      return Form(
+                child: Obx(() {
+                  return Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 50.0,
+                          bottom: 10,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'DADOS DO USUÁRIO',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Form(
                         key: _formKey,
                         child: Column(
                           children: [
@@ -47,22 +74,53 @@ class _ProfilePageState extends GalegosState<ProfilePage, GalegosDrawerControlle
                               label: controller.name.string,
                               controller: _newNameEC,
                               isSelected: controller.isSelected,
-                              onPressed: controller.isSelect,
+                              validator: Validatorless.required('Campo obrigatório'),
+                            ),
+                            ProfileData(
+                              title: 'CPF/CNPJ:',
+                              label: controller.valueCpfOrCnpj.value,
+                              isSelected: false,
                             ),
                             ProfileData(
                               title: 'Senha:',
-                              label: '' ?? controller.password.string,
+                              label: controller.password.value,
                               controller: _newPasswordEC,
                               isSelected: controller.isSelected,
-                              onPressed: controller.isSelect,
                               obscure: controller.isObscure,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('Campo obrigatório'),
+                                Validatorless.min(6, 'Mínimo 6 caracteres')
+                              ]),
+                            ),
+                            ProfileData(
+                              title: 'Confirmar Senha:',
+                              label: controller.password.value,
+                              isSelected: controller.isSelected,
+                              obscure: controller.isObscure,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('Campo obrigatório'),
+                                Validatorless.compare(_newPasswordEC, 'Senhas precisam ser iguais')
+                              ]),
                             ),
                           ],
                         ),
-                      );
-                    }),
-                  ],
-                ),
+                      ),
+                      Visibility(
+                        visible: controller.isSelected == true,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: GalegosButtonDefault(
+                              label: 'Atualizar',
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                }),
               ),
             ),
           );
