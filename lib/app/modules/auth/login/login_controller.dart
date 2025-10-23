1e0630e6a55ff1593d07d6d251aa3900fb043415
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-23 13:23:15 -0300

    feat: Criado método para atualizar os dados do usuario

--- lib/app/modules/auth/login/login_controller.dart
+++ lib/app/modules/auth/login/login_controller.dart
@@ -1,3 +1,5 @@
+// import 'dart:developer';
+
 import 'dart:developer';
 
 import 'package:get/get.dart';
@@ -6,6 +8,7 @@ import 'package:restaurante_galegos/app/core/constants/constants.dart';
 import 'package:restaurante_galegos/app/core/masks/mask_cpf.dart';
 import 'package:restaurante_galegos/app/core/mixins/loader_mixin.dart';
 import 'package:restaurante_galegos/app/core/mixins/messages_mixin.dart';
+import 'package:restaurante_galegos/app/repositories/auth/auth_repository_impl.dart';
 import 'package:restaurante_galegos/app/services/auth/auth_services.dart';
 
 class LoginController extends GetxController with LoaderMixin, MessagesMixin {
@@ -37,11 +40,20 @@ class LoginController extends GetxController with LoaderMixin, MessagesMixin {
   }) async {
     try {
       _loading.toggle();
-      log('LOADINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG $_loading');
       final userLogger = await _authServices.login(isCpf: isCpf, value: value, password: password);
       final storage = GetStorage();
       storage.write(Constants.USER_KEY, userLogger.id);
       _loading.toggle();
+    } on AuthException catch (e, s) {
+      log('Falha no login', error: e, stackTrace: s);
+      _message(
+        MessageModel(
+          title: 'Falha no login',
+          message:
+              'CPF/CNPJ ou senha incorreta', // pode ser "Senha incorreta" ou outro texto vindo do service
+          type: MessageType.error,
+        ),
+      );
     } catch (e) {
       _loading.toggle();
       _message(
