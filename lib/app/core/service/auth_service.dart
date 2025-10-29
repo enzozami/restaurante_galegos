104f9543f230497b64343e31f0f50b6d7a5ca402
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-29 10:17:58 -0300

    feat: Implementação do horário no app, ele não funciona fora do horário de funcionamento do estabelecimento

--- lib/app/core/service/auth_service.dart
+++ lib/app/core/service/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:restaurante_galegos/app/core/constants/constants.dart';
@@ -6,21 +7,40 @@ class AuthService extends GetxService {
   final _isLogged = RxnBool();
   final _getStorage = GetStorage();
 
+  bool canUseApp() {
+    var timeNow = DateTime.now();
+
+    final inicio = DateTime(timeNow.year, timeNow.month, timeNow.day, 9, 0);
+    final fim = DateTime(timeNow.year, timeNow.month, timeNow.day, 14, 50);
+
+    return inicio.isBefore(timeNow) && fim.isAfter(timeNow);
+  }
+
   Future<AuthService> init() async {
-    _getStorage.listenKey(Constants.USER_KEY, (value) {
-      _isLogged(value != null);
-    });
+    if (canUseApp()) {
+      _getStorage.listenKey(Constants.USER_KEY, (value) {
+        _isLogged(value != null);
+      });
 
-    ever(_isLogged, (isLogged) {
-      if (isLogged == null || !isLogged) {
-        Get.offAllNamed('/auth/login');
-      } else {
-        Get.offAllNamed('/home');
-      }
-    });
+      ever(_isLogged, (isLogged) {
+        if (isLogged == null || !isLogged) {
+          Get.offAllNamed('/auth/login');
+        } else {
+          Get.offAllNamed('/home');
+        }
+      });
 
-    _isLogged(getUserId() != null);
-    return this;
+      _isLogged(getUserId() != null);
+      return this;
+    } else {
+      Get.snackbar(
+        'Fora do horário de funcionamento',
+        'Nós funcionamos das 09:00 às 14:50h!',
+        duration: 3.seconds,
+        backgroundColor: Colors.amberAccent,
+      );
+      return this;
+    }
   }
 
   void logout() => _getStorage.write(Constants.USER_KEY, null);
