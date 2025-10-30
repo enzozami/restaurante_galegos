1bc1172ecafed44d0efe0263c340c35bfbb42e7c
Enzo Zamineli <enzozamineli@gmail.com>
2025-10-30 15:34:28 -0300

    feat: android sendo configurado

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:restaurante_galegos/app/core/ui/widgets/galegos_button_default.dart';
 import './splash_controller.dart';
 
 class SplashPage extends GetView<SplashController> {
@@ -15,15 +14,10 @@ class SplashPage extends GetView<SplashController> {
           children: [
             Image.network(
               'https://restaurantegalegos.wabiz.delivery/stores/restaurantegalegos/img/homeLogo.png?vc=20250915111500&cvc=',
-              width: Get.width,
-              height: 300,
+              fit: BoxFit.cover,
             ),
             const SizedBox(
               height: 10,
-            ),
-            GalegosButtonDefault(
-              label: 'Acessar',
-              onPressed: () => controller.checkLogged(),
             ),
           ],
         ),
