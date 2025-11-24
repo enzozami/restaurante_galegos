6bd79c21b95b45a799cd206360eb400bf28d9645
Enzo Zamineli <enzozamineli@gmail.com>
2025-11-24 08:36:17 -0300

    ci: Configurando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:intl/intl.dart';
 import 'package:restaurante_galegos/app/app_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:restaurante_galegos/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: '.env');
   final locale = Intl.getCurrentLocale();
   await initializeDateFormatting(locale, null);
